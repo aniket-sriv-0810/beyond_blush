92838eea6d5c2fc816a70b6f073b3bfc12130a00
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2025-07-08 13:06:30 +0530

    routes updated  !

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './pages/Home'
 import FloatingActionBtn from './components/FloatingActionBtn/FloatingActionBtn'
 import LoadingScreen from './components/Loaders/LoadingScreen'
 import { Route, Routes } from 'react-router-dom'
+import AboutSection from './components/About/AboutSection'
+import FAQs from './components/FAQs/FAQs'
 
 const App = () => {
    const [loading, setLoading] = useState(true);
@@ -16,11 +18,11 @@ const App = () => {
     <div>
     <FloatingActionBtn/>
     <Routes>
-      {loading ?
-      <Route path='/load' element={<LoadingScreen/>} />
-      :
-      <Route path='/' element={<Home/>} />
-      }
+     <Route path='/' element={<Home/>} />
+     <Route path='/about' element={<AboutSection/>} />
+     <Route path='/faqs' element={<FAQs/>} />
+     <Route path='/' element={<Home/>} />
+     <Route path='/' element={<Home/>} />
     </Routes>
       
     </div>
